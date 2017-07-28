7b53707f6b131ad73f94d430e44d2ef0165394a1
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-07-29 00:58:59 +0430

    Test bug fixed

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var HandyStorage = require('../lib/index')
 
-const storage = new HandyStorage('./db.json')
+const storage = new HandyStorage('./test/db.json')
 
 storage.json.a = 1
 storage.json.b = 1
